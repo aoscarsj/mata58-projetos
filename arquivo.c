148d89e23e25ae802f5bbea1d81c763468e4e850
Luiz C <luizdantas.cavalcanti@gmail.com>
2020-09-18 18:19:27 -0300

    Adicionada lista de erros a serem incluidos

--- arquivo.c
+++ arquivo.c
@@ -13,6 +13,33 @@ int erro(int tipoDeErro, int arquivoOrigem, int arquivoDestino){
     // ENOTENOUGHARGS é definido como erro 0, e significa que não foi passado
     // o número correto de argumentos para o programa
 
+    /*
+    A INCLUIR
+
+
+    #define EIO              5      / I/O error /
+    #define EBADF            9      / Bad file number /
+    #define EAGAIN          11      / Try again /
+    #define ENOMEM          12      / Out of memory /
+    #define EACCES          13      / Permission denied /
+    #define EFAULT          14      / Bad address /
+    #define EBUSY           16      / Device or resource busy /
+    #define EEXIST          17      / File exists /
+    #define EISDIR          21      / Is a directory /
+    #define EINVAL          22      / Invalid argument /
+    #define ENFILE          23      / File table overflow /
+    #define EMFILE          24      / Too many open files /
+    #define ETXTBSY         26      / Text file busy /
+    #define EFBIG           27      / File too large /
+    #define ENOSPC          28      / No space left on device /
+    #define EROFS           30      / Read-only file system /
+    #define ENAMETOOLONG    36      / File name too long /
+    #define EWOULDBLOCK     EAGAIN  / Operation would block /
+    #define ENOSTR          60      / Device not a stream /
+    #define EBADFD          77      / File descriptor in bad state /
+    #define ERESTART        85      / Interrupted system call should be restarted /
+    */
+    
     if(tipoDeErro == EPERM){
         printf("Operação não permitida\n");
     }
