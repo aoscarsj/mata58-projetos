82a44dbbc6d2df57ade0ea3ee4cad18ef5fe21bb
luizdantas.cavalcanti@gmail.com <luizdantas.cavalcanti@gmail.com>
2020-09-22 16:21:52 -0300

    Removida funcao 'erro' em arquivo.c

--- arquivo.c
+++ arquivo.c
@@ -11,142 +11,6 @@
 
 typedef enum{false,true} bool;
 
-//fechar arquivos, com prevencao de erros
-
-int erro(int arquivoOrigem, int arquivoDestino,
-         char const * nomeArquivoOrigem, char const * nomeArquivoDestino){
-    /*
-        A função "erro" recebe as informações sobre o estado das variáveis re-
-        ferentes aos arquivos, avalia o valor da variável global errno e imprime
-        uma mensagem de erro adequada, fechando os file descriptors e encerrando
-        o programa se necessário. Alguns erros são apenas notificados, mas não
-        causam encerramento do programa.
-    */
-
-    bool erroFatal = true;
-    // caso seja atribuído "false" o erro será ignorado, porém ainda assim
-    // será sinalizado
-
-    switch (errno) {
-        case ENOSPC:
-            printf("Erro: não há espaço livre no dispositivo de armazenamento.\n");
-            break;
-
-        case ENFILE:
-            // nunca ocorre no GNU, mas está definido devido ao POSIX
-            printf("Erro: o limite de arquivos abertos no sistema ");
-            printf("foi atingido.\n");
-            break;
-
-        case EMFILE:
-            printf("Erro: o limite de arquivos abertos para esse processo ");
-            printf("foi atingido.\n");
-            break;
-
-        case ENAMETOOLONG:
-            printf("Erro: não foi possível ");
-            if (arquivoOrigem < 0){
-                printf("abrir o arquivo %s ", nomeArquivoOrigem);
-            }else if (arquivoDestino < 0){
-                printf("criar o arquivo %s ", nomeArquivoDestino);
-            }
-            printf("pois o nome do arquivo é muito longo.\n");
-            break;
-
-        case EACCES:
-            printf("Erro: o usuário não tem acesso a esse diretório.\n");
-            break;
-
-        case EROFS:
-            printf("Erro: o sistema de arquivos é de somente leitura.\n");
-            break;
-
-        case ENOMEM:
-            printf("Erro: memória de sistema insuficiente para a operação.\n");
-            break;
-
-        case EBUSY:
-            printf("Erro: dispositivo ou recurso estão ocupados.\n");
-            erroFatal = false;
-            break;
-
-        case EFBIG:
-            printf("Erro: o arquivo %s não pode ser ", nomeArquivoOrigem);
-            printf("copiado pois seu tamanho é maior do que o máximo ");
-            printf("permitido pelo sistema de arquivos.\n");
-            break;
-
-        case EBADF:
-            printf("Erro: o descriptor do arquivo é inadequado.\n");
-            break;
-
-        case ETXTBSY:
-            printf("Erro: um dos arquivos está sendo utilizado no momento ");
-            printf("por outro programa.\n");
-            erroFatal = false;
-            break;
-
-        case ENOTENOUGHARGS:
-            // ENOTENOUGHARGS foi definido como erro 256, e significa que não foi passado
-            // o número correto de argumentos para o programa
-            printf("Erro: o programa foi invocado incorretamente. ");
-            printf("O programa deve ser invocado da seguinte forma:\n\n");
-            printf("filecopy ArquivoOrigem ArquivoDestino\n\n");
-            printf("Nenhum arquivo foi copiado. ");
-            break;
-
-        case EPERM:
-            printf("Erro: Operação não permitida ao usuário.\n");
-            break;
-
-        case ENOENT:
-            printf("Erro: Não foi possível abrir o arquivo %s: ", nomeArquivoOrigem);
-            printf("Arquivo ou diretório não existe.\n");
-            break;
-
-        case EINTR:
-            printf("Erro: chamada de sistema interrompida.\n");
-            erroFatal = false;
-            break;
-
-        case EEXIST:
-            printf("Erro: Não foi possível copiar o arquivo ");
-            printf("%s para o arquivo %s\n", nomeArquivoOrigem,nomeArquivoDestino);
-            printf("pois o arquivo %s já existe.\n", nomeArquivoDestino);
-            break;
-
-        case EISDIR:
-            printf("Erro: Não foi possível abrir o arquivo ");
-            if (arquivoOrigem < 0) {
-                printf("%s pois %s ", nomeArquivoOrigem, nomeArquivoOrigem );
-            }else if(arquivoDestino < 0){
-                printf("%s pois %s ", nomeArquivoDestino, nomeArquivoDestino);
-            }
-            printf("é um diretório e não um arquivo.\n");
-            break;
-
-        default:
-            perror("Um erro ocorreu");
-            printf("\n");
-            break;
-    }
-
-    if (erroFatal){
-        printf("O programa será encerrado.\n");
-        if (arquivoOrigem >= 0) {
-            close(arquivoOrigem);
-        }
-        if (arquivoDestino >= 0){
-            close(arquivoDestino);
-        }
-    }
-
-    return -1;
-}
-
-
-
-
 
 int fileCopy(const char * nomeOriginal, const char * nomeCopia){
     /*
@@ -164,7 +28,7 @@ int fileCopy(const char * nomeOriginal, const char * nomeCopia){
     // O_RDONLY -> modo somente leitura ao abrir o arquivo
     // valor de retorno -1 sinaliza que um erro ocorreu
     if((arquivoOrigem = open(nomeOriginal, O_RDONLY)) == -1){
-        return erro(arquivoOrigem,arquivoDestino,nomeOriginal,nomeCopia);
+        return -1;
     }
 
     /*
@@ -178,7 +42,7 @@ int fileCopy(const char * nomeOriginal, const char * nomeCopia){
     do mesmo grupo e quaisquer outros podem ler, escrever e executar o arquivo.
     */
     if((arquivoDestino = open(nomeCopia, O_WRONLY | O_CREAT | O_EXCL, 0777)) == -1){
-        return erro(arquivoOrigem, arquivoDestino,nomeOriginal,nomeCopia);
+        return -1;
     }
 
 
@@ -198,21 +62,23 @@ int fileCopy(const char * nomeOriginal, const char * nomeCopia){
         }else if (errno != EINTR){
             // ignora o erro EINTR, pois a leitura pode ser tentada novamente
             // sem perdas
-            return erro(arquivoOrigem, arquivoDestino, nomeOriginal, nomeCopia);
+            nBytesLidos = -1; // registra que um erro ocorreu e não leu
+                              // o arquivo corretamente.
+            break;
         }
 
 
     }
 
     // rotina de encerramento, fechando os arquivos
-    if (nBytesLidos == 0){
-        if (close(arquivoDestino) < 0){
-            return erro(arquivoOrigem, arquivoDestino,nomeOriginal,nomeCopia);
-        }
-        if (close(arquivoOrigem) < 0){
-            return erro(arquivoOrigem, arquivoDestino,nomeOriginal,nomeCopia);
-        }
+    if (close(arquivoDestino) < 0){
+        return -1;
+    }
+    if (close(arquivoOrigem) < 0){
+        return -1;
+    }
 
+    if (nBytesLidos == 0){
         return nBytesEscritosTotal;
     }
 
--- treecopy.c
+++ treecopy.c
@@ -16,10 +16,31 @@ int erroTreeCopy(int dirOrigem, int dirDestino,
          char const * nomeDirOrigem, char const * nomeDirDestino);
 unsigned long long totalBytes = 0;
 unsigned int totalFiles = 0, totalDirs = 0;
-
+int treeCopy(char *originalPath, char *destinyPath);
 /*
     Função utilizada para realizar a copia recursiva do diretorio selecionado para outro
 */
+
+int main(int argc, char *argv[]){
+    if (argc != 3){
+        errno = ENOTENOUGHARGS;
+        return erroTreeCopy(-1,-1,"","");
+        //  esses argumentos para a função erro() sinalizam que nenhum arquivo
+        //  foi aberto.
+    }
+    char * pastaOrigem = argv[1];
+    char * pastaDestino = argv[2];
+
+    if(!mkdir(pastaDestino,MAXFILEPERMS)){
+        treeCopy(pastaOrigem, pastaDestino);
+        printf("treecopy: foram copiados %i diretórios, %i arquivos e %llu bytes de %s para %s\n", totalDirs, totalFiles, totalBytes, pastaOrigem, pastaDestino);
+
+    }else{
+        erroTreeCopy(-1,-1,pastaOrigem,pastaDestino);
+    }
+    return 0;
+}
+
 int treeCopy(char *originalPath, char *destinyPath){
     DIR *origem = opendir(originalPath);
     /*
@@ -60,26 +81,6 @@ int treeCopy(char *originalPath, char *destinyPath){
     }
 
     return 1;
-}
-
-int main(int argc, char *argv[]){
-    if (argc != 3){
-        errno = ENOTENOUGHARGS;
-        return erroTreeCopy(-1,-1,"","");
-        //  esses argumentos para a função erro() sinalizam que nenhum arquivo
-        //  foi aberto.
-    }
-    char * pastaOrigem = argv[1];
-    char * pastaDestino = argv[2];
-
-    if(!mkdir(pastaDestino,MAXFILEPERMS)){
-        treeCopy(pastaOrigem, pastaDestino);
-        printf("treecopy: foram copiados %i diretórios, %i arquivos e %llu bytes de %s para %s\n", totalDirs, totalFiles, totalBytes, pastaOrigem, pastaDestino);
-
-    }else{
-        erroTreeCopy(-1,-1,pastaOrigem,pastaDestino);
-    }
-    return 0;
 }
 
 int erroTreeCopy(int dirOrigem, int dirDestino,
