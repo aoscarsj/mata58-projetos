b348cc382b88fba6afee04b02aa39224ab42df3e
luizdantas.cavalcanti@gmail.com <luizdantas.cavalcanti@gmail.com>
2020-09-22 11:39:52 -0300

    Eliminada funcao createDir e inserida const PATH_MAX

--- treecopy.c
+++ treecopy.c
@@ -1,4 +1,5 @@
 #include <errno.h>
+#include <linux/limits.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/types.h>
@@ -9,7 +10,7 @@
 #include "arquivo.c"
 #define FILE 8
 #define ENOTENOUGHARGS 256
-int createDir(char *path);
+#define MAXFILEPERMS 0777
 unsigned long long totalBytes = 0;
 unsigned int totalFiles = 0, totalDirs = 0;
 
@@ -19,7 +20,7 @@ unsigned int totalFiles = 0, totalDirs = 0;
 int treeCopy(char *originalPath, char *destinyPath){
     DIR *origem = opendir(originalPath);
     /*
-    * 
+    *
     */
     if(origem){
         // abriu o diretório de origem
@@ -27,15 +28,15 @@ int treeCopy(char *originalPath, char *destinyPath){
 
         struct dirent *child = readdir(origem);
         while( child != NULL){
-            
+
             // enquanto o diretório tiver arquivo ou pasta, continue executando.
-            
-                char newPathOriginal[500];
-                char newPathDestiny[500];
+
+                char newPathOriginal[PATH_MAX];
+                char newPathDestiny[PATH_MAX];
                 strcpy(newPathOriginal, originalPath);
                 strcat(newPathOriginal, "/");
                 strcat(newPathOriginal, child->d_name);
-                
+
 
                 strcpy(newPathDestiny, destinyPath);
                 strcat(newPathDestiny, "/");
@@ -43,12 +44,12 @@ int treeCopy(char *originalPath, char *destinyPath){
 
             if(child->d_type == FILE){
                 totalFiles++;
-                totalBytes += fileCopy(newPathOriginal, newPathDestiny); 
+                totalBytes += fileCopy(newPathOriginal, newPathDestiny);
             }else if(strcmp(child->d_name, ".") !=0 && strcmp(child->d_name, "..")  != 0){
                 totalDirs++;
-                //Não pode ser um arquivo nem a pasta . e nem a pasta .. 
-                createDir(newPathDestiny);
-                
+                //Não pode ser um arquivo nem a pasta . e nem a pasta ..
+                mkdir(newPathDestiny,MAXFILEPERMS);
+
                 treeCopy(newPathOriginal, newPathDestiny);
             }
             child = readdir(origem);
@@ -56,10 +57,6 @@ int treeCopy(char *originalPath, char *destinyPath){
     }
 
     return 1;
-}
-
-int createDir(char *path){
-    return mkdir(path, 0777); 
 }
 
 int main(int argc, char *argv[]){
@@ -72,10 +69,10 @@ int main(int argc, char *argv[]){
     char * pastaOrigem = argv[1];
     char * pastaDestino = argv[2];
 
-    if(!createDir(pastaDestino)){
+    if(!mkdir(pastaDestino,MAXFILEPERMS)){
         treeCopy(pastaOrigem, pastaDestino);
         printf("treecopy: foram copiados %i diretórios, %i arquivos e %llu bytes de %s para %s\n", totalDirs, totalFiles, totalBytes, pastaOrigem, pastaDestino);
-        
+
     }
     return (0);
 }
