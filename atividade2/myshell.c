31572ad03e7ed087b8e55907fea9314cb3017802
Luiz Cavalcanti <luizdantas.cavalcanti@gmail.com>
2020-09-26 17:43:39 -0300

    Escrito loop principal

--- atividade2/myshell.c
+++ atividade2/myshell.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <errno.h>
 
+#define MAX_CHARS 4096
 /* FUNÇÕES A SE IMPLEMENTAR
+erroEntrada() // imprime mensagem de erro relacionada à entrada
 start()
 wait()
 waitfor()
@@ -14,22 +17,36 @@ pwd()
 
 int main(int argc, char const *argv[]) {
     //  TODO:rotina de inicialização
-    
-    /* TODO:  LOOP PRINCIPAL
+    char raw_line[MAX_CHARS + 1]; // 4096 chars + null
+    char * args[100];
+    while(true) {
         printf("myshell> "), fflush(stdout); // imprime e força a saída imediata na tela
-        // ler entrada (pode ser vazio (apenas apertar enter), comandos, ou EOF) e extrair seu conteudo (separar palavras) com strtok
-        // se for EOF, "exit" ou "quit" encerrar o loop e encerrar retornando 0
-        // se for em branco: continue;
-        ////
+        if(fgets(raw_line, sizeof raw_line ,stdin) && raw_line[0] != '\n'){
+            /*
+            ////
             se for comando, executar o comando com os argumentos passados
             o shell precisa monitorar os comandos que executou para reportar
             o status de saída, e também com os comandos wait, waitfor e watchdog
             ele acompanha a execução de um processo ate o fim
-        ////
-        // se foi algo incorreto (comando inexistente, etc) imprimir mensagem de erro
+            ////
+            // se foi algo incorreto (comando inexistente, etc) imprimir mensagem de erro
+            // se for "exit" ou "quit" encerrar o loop e encerrar retornando 0
+            */
+        }else if (feof(stdin)){
+            errno == 0;
+            break;
+        }else if (raw_line[0] == '\n'){
+            continue;
+            // se for em branco: continue;
+        }else{
+            erroEntrada(); // imprime mensagem de erro correspondente
+        }
+    }
 
-    */
+    if (errno != 0)
+        return -1;
 
+    return 0;
     // se encerrou com quit, exit ou EOF return 0;
     // se aconteceu algum erro, retornar -1
 }
